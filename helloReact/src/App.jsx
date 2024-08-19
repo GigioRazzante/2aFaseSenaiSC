511fe41354d7828d6be06a7f1d37fa9ff43a72f9
GigioRazzante <giovani_ra_silva@estudante.sesisenai.org.br>
2024-08-19 17:07:44 -0300

    Adcionado helloReact

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='titulo1'>Hello React JS</h1>
+      <p className='paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quos blanditiis expedita nostrum error, saepe alias sit quae animi quo. Autem, animi minima recusandae voluptate deleniti voluptatibus tempora illo error.</p>
+      <p className='paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quos blanditiis expedita nostrum error, saepe alias sit quae animi quo. Autem, animi minima recusandae voluptate deleniti voluptatibus tempora illo error.</p>
+      <p className='paragrafo'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quos blanditiis expedita nostrum error, saepe alias sit quae animi quo. Autem, animi minima recusandae voluptate deleniti voluptatibus tempora illo error.</p>
+    </>
+  )
+}
+
+export default App
