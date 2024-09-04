a45ab3391812f00055c6d13fd0d46c3f21bc4668
GIOVANI RAZZANTE ABBAMONTE DA SILVA <giovani_ra_silva@estudante.sesisenai.org.br>
2024-09-04 17:06:03 -0300

    Ex 526 quase pronto

--- propsPages/src/pages/Exercicios.jsx
+++ propsPages/src/pages/Exercicios.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import Titulo from "../components/Titulo"
+import './Exercicio.css'
 
 function Exercicios() {
 
@@ -13,30 +14,101 @@ function Exercicios() {
         let moedas10= Number(prompt('Quantas moedas de R$0,10?'))
         let moedas5= Number(prompt('Quantas moedas de R$0,05?'))
 
-        let somaMoedas= ((moedas1= moedas1*1) + (moedas50= moedas50*0.50)+ (moedas25= moedas25*0.25)+ (moedas10= moedas10* 0.10)+ (moedas5= moedas5*0.05))
+        let somaMoedas= ((moedas1= moedas1*1) + (moedas50= moedas50*0.50)+ (moedas25= moedas25*0.25)+ (moedas10= moedas10* 0.10)+ (moedas5= moedas5*0.05)).toFixed(2)
 
-        let diferencaPreco= somaMoedas - precoProduto
+        let diferencaPreco= (somaMoedas - precoProduto).toFixed(2)
         if(diferencaPreco < 0 ){
 
-            setResultado('Você não pode comprar porque não tem dinheiro suficiente no porquinho:(\nDinheiro em moedas: R$ '+somaMoedas+'\nPreço do produto R$: '+precoProduto+'\nValor faltando R$: '+diferencaPreco)
+            setResultado(`Você não pode comprar:( \nDinheiro no porquinho R$: ${somaMoedas} Preço do produto R$: ${precoProduto} \nQuantia que falta R$: ${diferencaPreco}`)
         }else{
 
-            setResultado('Você não pode comprar porque não tem dinheiro suficiente no porquinho:(\nDinheiro em moedas: R$ '+somaMoedas+'\nPreço do produto R$: '+precoProduto+'\nValor faltando R$: '+diferencaPreco)
+          setResultado(`Você pode comprar:) \nDinheiro no porquinho R$: ${somaMoedas} Preço do produto R$: ${precoProduto} \nQuantia que sobra R$: ${diferencaPreco}`)
         }
         
 
        
 
     }
+    const [notas, setNotas]= useState()
+    function rodar526(){
+
+      let vetorNotas=[]
+      let vetorPesoNotas= []
+
+      for(let i=0; i < 4; i++){
+
+        let notasDigitadas
+        do{
+
+          notasDigitadas=Number(prompt('Digite suas 4 notas: (nota1, nota2, nota3, nota4) '))
+
+        }while(notasDigitadas> 10)
+
+        vetorNotas.push(notasDigitadas)
+        
+       }
+       
+      for( let i=0; i < 4; i++){
+       
+        let pesoNotasDigitadas
+        pesoNotasDigitadas= Number(prompt(`Digite o peso das suas respectivas notas: ${vetorNotas}`))
+        vetorPesoNotas.push(pesoNotasDigitadas)
+
+      }
+
+      let nota1= vetorNotas[0]
+      let nota2= vetorNotas[1]
+      let nota3= vetorNotas[2]
+      let nota4= vetorNotas[3]
+      let somaNotas= (nota1+nota2+nota3+nota4)
+
+      let pesoNota1= vetorPesoNotas[0]
+      let pesoNota2= vetorPesoNotas[1]
+      let pesoNota3= vetorPesoNotas[2]
+      let pesoNota4= vetorPesoNotas[3]
+      let somaPesoNotas= (pesoNota1+pesoNota2+pesoNota3+pesoNota4) 
+      
+      let mediaNotas= (((nota1*pesoNota1)+(nota2*pesoNota2)+(nota3*pesoNota3)+(nota4*pesoNota4))/somaPesoNotas).toFixed(2) 
+
+      if(mediaNotas>=7){
+      
+        setNotas(`APROVADO Notas: ${vetorNotas} Peso Notas: ${vetorPesoNotas} Média ponderada: ${mediaNotas}`)
+        
+      }else{
+
+        setNotas(`REPROVADO Notas: ${vetorNotas} Peso Notas: ${vetorPesoNotas} Média ponderada: ${mediaNotas}`)
+
+      }
+      
+
+
+
+
+     
+
+  }
 
   return (
     <div className="porquinho-container">
 
     <Titulo text={"Porquinho"} emoji={"🐖"}/>
 
-    <button onClick={rodar525}>Exercício 5.25</button>
-    {resultado}
+    <div className="elementoBtnExercicio">
+
+    <button className="btnExercicio" onClick={rodar525}>Exercício 5.25</button>
+    <button className="btnExercicio" onClick={rodar526}>Exercício 5.26</button>
+
+    </div>
+
+    <div className="setTexto">
+
       
+
+    </div>
+    
+    {resultado}
+    {notas}
+     
     </div>
   )
 }
